0a3db4304e020e9eb80d6661de41b08976d69087
Andy Young <and.you.dev@gmail.com>
2022-05-10 19:55:59 +0100

    anim homepage

--- index.js
+++ index.js
@@ -3,3 +3,34 @@
 let thisYear = new Date();
 
 $("#dynYear").text(thisYear.getFullYear());
+
+let imgId = 2;
+
+
+
+$(document).ready(function(){
+
+  myFadeAnim(1);
+  setInterval(function() {
+    if(imgId > 4) {
+        imgId = 1;
+    }
+
+    myFadeAnim(imgId);
+    imgId++;
+
+  }, 8000);
+
+
+
+
+});
+
+function myFadeAnim (id) {
+  let imgId = "#landing-graphic" + id;
+
+//  $(imgId).fadeIn(2500).fadeOut(2500);
+  //$("#anim-logo").show().fadeTo(1000, 0.2);
+  $(imgId).fadeIn(4000).fadeOut(4000);
+
+}
